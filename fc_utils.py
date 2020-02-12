5043455219812aafa1f98123ca721c59da08055f
Raihaan Patel <patelraihaan@gmail.com>
2020-02-12 17:40:37 -0500

    added options for computing covariance and inverse covariance to get_fc_mx fcn

--- fc_utils.py
+++ fc_utils.py
@@ -1,7 +1,18 @@
 import numpy as np
+from sklearn.covariance import GraphicalLassoCV
 
 def get_fc_mx(tseries, method='corr'):
     #Return a connectivity matrix, using one of three methods
     #Expects tseries to have dimensions regions x time
-    fc=np.corrcoef(tseries)
+
+ 
+    if method=='corr':
+        fc = np.corrcoef(tseries)
+    elif method=='cov':
+        fc = np.cov(tseries)
+    elif method=='precision':
+        #the GraphicalLasso fcn wants time x regions dimensions
+        estimator = GraphicalLassoCV()
+        estimator.fit(np.transpose(tseries))
+        fc = estimator.covariance_
     return fc
