9bada3adc053dde37b56d0e1b726811c5719a375
raihaan <patelraihaan@gmail.com>
2020-02-12 16:34:50 -0500

    Delete fc_utils.py

--- fc_utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def get_fc_mx(tseries, method='corr'):
-    #Return a connectivity matrix, using one of three methods
-    #Expects tseries to have dimensions regions x time
-    fc=np.corrcoef(tseries)
-    return fc
